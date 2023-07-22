49a638bbae858db4b6579ac082f257f2d639cdce
manny <onegoal711@gmail.com>
2023-07-21 22:28:51 -0700

    fix: active status

--- server/api/rooms/index.ts
+++ server/api/rooms/index.ts
@@ -2,7 +2,7 @@ import { Room } from '~/models/room';
 
 export default defineEventHandler(async (event) => {
     if (getMethod(event) === 'GET') {
-        return await Room.find()
+        return await Room.find({ active: true })
         // const { visitor_id } = getQuery(event)
         // const res = await fetch(`https://www.roomvo.com/services/room/rooms/?visitor=${visitor_id}&room_type=vendor&product_type__in=1%2C4%2C5&locale=en-gb`)
         //     .then(res => res.json());
